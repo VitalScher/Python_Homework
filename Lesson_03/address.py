8e6e1e5e56cb665e4999063990f51a6ad756bef8
VitalScher <vitsch22@gmail.com>
2026-06-17 22:36:48 +0300

    Рев.2. Скрипты для ДЗ из урока 5.3 на проверку

--- Lesson_03/address.py
+++ Lesson_03/address.py
@@ -1,4 +1,4 @@
-class Adress:
+class Address:
     def __init__(self, index, city, street, building, flat):
         self.index = index
         self.city = city
--- Lesson_03/lesson_3_task_3.py
+++ Lesson_03/lesson_3_task_3.py
@@ -1,8 +1,8 @@
-from adress import Adress
+from address import Address
 from mailing import Mailing
 
-mailing = Mailing(to_adress = Adress("100200", "Энск", "ул. Прямая", "д.5", "кв.2"),
-                  from_adress = Adress("200400", "Уйск", "ул. Кривая", "д.53", "кв.124"),
+mailing = Mailing(to_address = Address("100200", "г.Энск", "ул.Прямая", "д.5", "кв.2"),
+                  from_address = Address("200400", "г.Уйск", "ул.Кривая", "д.53", "кв.124"),
                   cost = 100.2, 
                   track = "7654532")
 
